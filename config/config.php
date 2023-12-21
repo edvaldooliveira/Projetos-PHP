57c748aaddb1024c095c27fe092ff9222a7b1181
edvaldooliveira <52869737+edvaldooliveira@users.noreply.github.com>
2023-12-21 11:23:42 -0300

    alteração url_base

--- config/config.php
+++ config/config.php
@@ -10,5 +10,5 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://localhost:8000/');
+define('URL_BASE', 'http://localhost:8000/ead/Projetos-PHP/');
 //define('URL_BASE', 'http://localhost/estrutura_mvc/');
